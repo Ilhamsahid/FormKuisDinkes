e54e0fe855c5166ba52d9e7589145a3d2a722dd9
ilhamsahid <ilhamsahit988@gmail.com>
2025-12-17 13:13:13 +0700

    feat: membuat question view

---
+++ view/question-view.php
@@ -0,0 +1,34 @@
+<div>
+    <label class="block mb-2 text-sm font-medium text-gray-700">
+        Instansi
+    </label>
+    <input
+    type="text"
+    name="instansi"
+    placeholder="Contoh: SMKN 1 Probolinggo"
+    class="w-full rounded-lg border border-gray-300 px-4 py-2.5 text-sm
+            focus:border-blue-500 focus:ring-2 focus:ring-blue-500/30 outline-none"
+    />
+
+</div>
+
+<div class="flex gap-4">
+        <button
+        type="button"
+        onclick="prevStep()"
+        class="w-full rounded-lg border border-gray-300 px-4 py-2.5 text-sm font-medium text-gray-700
+            hover:bg-gray-100 transition"
+        >
+            ← Kembali
+        </button>
+
+        <!-- SUBMIT -->
+        <button
+            type="submit"
+            class="w-full rounded-lg bg-blue-600 px-4 py-2.5 text-sm font-medium text-white
+                hover:bg-blue-700 transition"
+        >
+            Kirim →
+        </button>
+
+</div>
