d1490398cc9cd8ddfd373d37575dc282f10a8d36
ilhamsahid <ilhamsahit988@gmail.com>
2025-12-17 13:39:03 +0700

    feat: memasukkan data ke form

---
+++ data/Pertanyaan.php
@@ -0,0 +1,24 @@
+<?php
+
+class Pertanyaan {
+    public $question = [
+        'Kemudahan akses informasi layanan  Dinas Kesehatan Pengendalian Penduduk dan KB Kota Probolinggo. (informasi layanan tersedia di berbagai media elektronik dan nonelektronik)',
+        'Kesesuaian persyaratan pelayanan dengan jenis pelayanannya',
+        'Bagaimana pendapat Saudara tentang kemudahan Sistem, mekanisme dan prosedur pelayanan',
+        'Bagaimana pendapat Saudara tentang kecepatan waktu dalam memberikan pelayanan?',
+        'Bagaimana pendapat Saudara tentang jam buka pelayanan sudah tepat waktu sesuai dengan Standar pelayanan yang dijanjikan?',
+        'Tarif atau biaya pelayanan yang diberikan sesuai dengan ketentuan',
+        'Bagaimana pendapat saudara tentang Kesesuaian produk pelayanan antara yang tercantum dalam standar pelayanan dengan hasil yang diberikan',
+        'Bagaimana pendapat saudara tentang penanganan pengaduan pengguna pelayanan',
+        'Bagaimana pendapat Saudara tentang kompetensi /kemampuan petugas dalam  layanan?',
+        'Bagaimana perilaku petugas dalam layanan terkait dengan kesopanan dan keramahan ?',
+        'Bagaimana pendapat Saudara tentang kualitas sarana dan prasarana pelayanan?',
+        'Bagaimana pendapat Saudara tentang transparansi pelayanan (informasi dan proses pelayanan transparan akuntabel)',
+        'Integritas petugas pelayanan, apakah berintegritas tinggi dan tidak melakukan KKN'
+    ];
+
+
+    public function getQuestion(){
+        return $this->question;
+    }
+}
